db7c3907fe91537931b54c6a797320f024ed8a0d
John Paul Pageler Jr <harakido1@yahoo.com>
2024-01-22 04:44:02 -0500

    Setup coalController and goalRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const express = require("express");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
