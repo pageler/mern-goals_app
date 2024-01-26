e9bd8e28462496eb63ad49e125c3cc93c5c3dfea
John Paul Pageler Jr <harakido1@yahoo.com>
2024-01-26 11:30:20 -0500

    REST api for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler"); // Await response from DB.
+const Goal = require("../models/goalModel");
 
 // @description: Read goals
 // @route: GET /api/goals
 // @access: Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Read goals." });
+    const goals = await Goal.find();
+
+    res.status(200).json(goals);
 });
 
 // @description: Create goal
@@ -17,21 +20,45 @@ const createGoal = asyncHandler(async (req, res) => {
         throw new Error("Please add a text field."); // Express error handler.
     }
 
-    res.status(200).json({ message: "Create goal." });
+    const goal = await Goal.create({
+        text: req.body.text,
+    });
+
+    res.status(200).json(goal);
 });
 
 // @description: Update goal
 // @route: PUT /api/goal/id
 // @access: Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400);
+        throw new Error("Goal not found.");
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+
+    res.status(200).json(updatedGoal);
 });
 
 // @description: Delete goals
 // @route: DELETE /api/goals/id
 // @access: Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400);
+        throw new Error("Goal not found.");
+    }
+
+    await goal.deleteOne();
+
+    res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
---
+++ backend/models/goalModel.js
@@ -0,0 +1,14 @@
+// Define resources using mongoose schema to define MongoDB object fields:
+const mongoose = require("mongoose");
+
+const goalSchema = mongoose.Schema(
+    {
+        text: {
+            type: "string",
+            required: [true, "Please add a text value"],
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Goal", goalSchema);
